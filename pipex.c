080664a573dcd45f284ca8a20dcf869223dd7d1c
Mohamed Bifenzi <mbifenzi@e1r1p7.1337.ma>
2021-11-09 13:36:14 +0100

    pipex complete

--- pipex.c
+++ pipex.c
@@ -6,22 +6,20 @@
 /*   By: mbifenzi <mbifenzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 15:07:45 by mbifenzi          #+#    #+#             */
-/*   Updated: 2021/11/08 18:32:12 by mbifenzi         ###   ########.fr       */
+/*   Updated: 2021/11/09 13:27:37 by mbifenzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	parent_process(int *fd, char **argv, char **env)
+void	child_process2(int *fd, char **argv, char **env)
 {
 	int outfile;
 
 	outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	dup2(outfile, 1);
 	dup2(fd[0], 0);
 	close(fd[1]);
-	dup2(outfile, 1);
-	if (outfile == -1)
-		error();
 	execute_exe(argv[3], env);
 }
 
@@ -35,28 +33,29 @@ void	child_process(int *fd, char **argv, char **env)
 	dup2(infile, 0);
 	if (infile == -1)
 		error();
+	close(infile);
 	execute_exe(argv[2], env);
-	// parent_process(fd, argv, env);
 }
 
 int main(int argc, char **argv, char **env)
 {
     int fd[2];
 	int pid;
+	int pid1;
 
 	if(argc != 5)
 		error();
 	if (pipe(fd) == -1)
 		return (0);
-	
 	pid = fork();
 	if (pid == 0)
 		child_process(fd, argv, env);
-	else
-	{
-	pid = fork();
-	if (pid == 0)
-		parent_process(fd, argv, env);
-	}
+	close(fd[1]);
+	pid1 = fork();
+	if (pid1 == 0)
+		child_process2(fd, argv, env);
+	close(fd[0]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid, NULL, 0);
 	return(0);
 }
---
+++ pipex_bonus.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbifenzi <mbifenzi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 16:18:07 by mbifenzi          #+#    #+#             */
+/*   Updated: 2021/11/09 12:19:00 by mbifenzi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	child_process(char *argv, char **envp)
+{
+	pid_t	pid;
+	int		fd[2];
+
+	if (pipe(fd) == -1)
+		error();
+	pid = fork();
+	if (pid == -1)
+		error();
+	if (pid == 0)
+	{
+		close(fd[0]);
+		dup2(fd[1], 1);
+		execute_exe(argv, envp);
+	}
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], 0);
+		waitpid(pid, NULL, 0);
+	}
+}
+
+void	here_doc(char *limiter)
+{
+	pid_t	pid;
+	int		fd[2];
+	char	*line;
+
+	if (pipe(fd) == -1)
+		error();
+	pid = fork();
+	if (pid == 0)
+	{	
+		close(fd[0]);
+		while (get_next_line(0, &line) != 0)
+		{
+			if(ft_strncmp(line, limiter,ft_strlen(limiter)) == 0)
+				exit (0);
+			ft_putstr_fd(line, fd[1]);
+			ft_putstr_fd("\n", fd[1]);
+		}
+	}
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], 0);
+		// wait(NULL);
+	}
+}
+
+int ft_open(char *file, int i)
+{
+	int	fd;
+
+	fd = 0;
+	if (i == 0)
+		fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	else if (i == 1)
+		fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0777);
+	else if (i == 2)
+		fd = open(file, O_RDONLY, 0777);
+	if (fd == -1)
+		error();
+	return (fd);
+}
+
+int main(int argc, char **argv, char **env)
+{
+    int i;
+	int infile;
+	int outfile;
+
+	if (argc < 5)
+		error();
+
+	if (ft_strncmp(argv[1], "here_doc", 8) == 0)
+	{
+		i  = 3;
+		outfile = ft_open(argv[argc - 1], 0);
+		here_doc(argv[2]);
+	}
+	else
+	{
+		i = 2;
+		outfile = ft_open(argv[argc - 1], 1);
+		infile = ft_open(argv[1], 0);
+		dup2(infile, 0);
+	}
+	while (i < argc - 2)
+		child_process(argv[i++], env);
+	dup2(outfile, 1);
+	execute_exe(argv[argc - 2], env);
+	return (0);
+}
